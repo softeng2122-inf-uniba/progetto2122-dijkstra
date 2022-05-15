93e4432dfcf3a089e4f656bec165309ede3f49a5
Chiara Margari <margarichiara2001@gmail.com>
2022-05-16 00:12:07 +0200

    Inizio della strutturazione della classe App e metodo Main

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -3,23 +3,56 @@ package it.uniba.app;
 /**
  * Main class of the application.
  */
-public final class App {
 
-    /**
-     * Get a greeting sentence.
-     *
-     * @return the "Hello World!" string.
-     */
-    public String getGreeting() {
-        return "Hello World!";
-    }
+//import java.util.Scanner;
 
-    /**
-     * Entrypoint of the application.
-     *
-     * @param args command line arguments
-     */
-    public static void main(final String[] args) {
-        System.out.println(new App().getGreeting());
+public final class App {          //implementazione classe App
+    private static String parolaSegreta= new String();
+    //Giocatore giocatore= new Giocatore();
+    
+    /*public void setParola(String x){
+        
+    }*/
+    
+    /*public static String getParola(){
+        
+    }*/
+    
+    /*public static void gioca(){
+        
+    }*/
+    
+    /*public static String getHelp(){
+        
+    }*/
+    
+   /* public static void esci(){
+        
+    }*/
+    
+
+   
+    public static void main(final String[] args) {     
+        /*if(args[0].equals("--help")|| args[0].equals("-h")){
+            String y= getHelp();
+        }
+        Scanner scanner= new Scanner(System.in);
+        String comando= scanner.nextLine();
+        Analizzatore analizzatore= new Analizzatore();
+        int x= analizzatore.analizzatoreComando(comando);
+        switch(x){
+            case 1: gioca();
+            break;
+            case 2: String y= getHelp();
+            break;
+            case 3: esci();
+            break;
+            case 4: String z= getParola();
+            break;
+            case 5: setParola(parolaSegreta);
+            break;
+            default: System.out.println("Errore nell'inserimento del comando");
+            break;
+        }*/
     }
 }
