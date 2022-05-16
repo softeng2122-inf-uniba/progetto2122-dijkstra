6e74a755e34e335f680c50ff1c75364e0f7a45f3
mattia1406 <m.marinelli36@studenti.uniba.it>
2022-05-16 17:26:06 +0200

    Aggiunta commenti sulla responsabilità della classe

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -2,6 +2,9 @@ package it.uniba.app;
 
 import java.util.Scanner;
 
+/** questa classe ha il compito di gestire le istanze della classe giocatore e permettere l'input all'utente **/
+/*<<entity>>**/
+
 public class Giocatore {
     public String input() {
         Scanner keyboard = new Scanner(System.in);
