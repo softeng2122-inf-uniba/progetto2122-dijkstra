197fada6b48fca6ced651c55f98001febd07eea0
mattia <m.marinelli36@studenti.uniba.it>
2022-05-16 14:55:33 +0200

    implementazione metodo 'input' nella classe giocatore

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -1,5 +1,9 @@
 package main.java.it.uniba.app;
 
 public final class Giocatore {
-    
+    public static String input() {
+        Scanner keyboard = new Scanner(System.in);
+        String userInput = keyboard.nextLine();
+        return userInput;
+    }
 }
