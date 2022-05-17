155b97c78b1ff76f07e83d135194cb5766d3dea7
Roby2851 <roby2435@gmail.com>
2022-05-17 09:42:28 +0200

    Prima bozza "analizzatoreTentativo"

--- src/main/java/it/uniba/app/Analizzatore.java
+++ src/main/java/it/uniba/app/Analizzatore.java
@@ -2,6 +2,40 @@ package it.uniba.app;
 
 public final class Analizzatore{
     
+    public enum Colore {GRIGIO,GIALLO,VERDE};
     
-    
+    public static Colore[] analizzatoreTentativo(String input,String parolaSegreta){
+        
+        Colore[] coloriCaratteri = new Colore[input.length()];
+        
+        for(int i=0;i<input.length();i++){
+            
+            coloriCaratteri[i]=Colore.GRIGIO;
+            
+        }
+        
+        for(int i=0;i<parolaSegreta.length();i++){
+            
+            for(int j=0;j<input.length();i++){
+                
+                if(input.charAt(j) == parolaSegreta.charAt(i) && i == j){
+                    
+                    coloriCaratteri[j]=Colore.VERDE;
+                    
+                }
+                
+                else if(input.charAt(j) == parolaSegreta.charAt(i) && i != j){
+                    
+                    coloriCaratteri[j]=Colore.GIALLO;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return coloriCaratteri;
+        
+    }
+     
 }
