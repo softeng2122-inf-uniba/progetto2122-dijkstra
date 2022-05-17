09c7401b6238f89f515a23f4bfb61dbf5c806e8a
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-05-17 11:27:23 +0200

    aggiunti return per reindirizzamento a funzioni corrispondenti a comando inserito

--- src/main/java/it/uniba/app/Analizzatore.java
+++ src/main/java/it/uniba/app/Analizzatore.java
@@ -38,25 +38,25 @@ public final class Analizzatore{
         
     }
 
-    public static void analizzatoreComando(String inputUser) throws InputUserNotValid {
+    public static int analizzatoreComando(String inputUser) throws InputUserNotValid {
 
         token = inputUser.trim().split(" ");
 
 		if (token.length <= 2) {
 
 			if (token[0].equalsIgnoreCase("/nuova")) {
-
+                return 1;
 			} else if (token[0].equalsIgnoreCase("/mostra")) {
-
+                return 2;
 			} else if (token[0].equalsIgnoreCase("/help") || token[0].equalsIgnoreCase("--h")
 					|| token[0].equalsIgnoreCase("-help")) {
-
+                return 3;
 			} else if (token[0].equalsIgnoreCase("/play")) {
-
+                return 4;
 			} else if (token[0].equalsIgnoreCase("/exit")) {
-
+                return 5;
 			} else if (token[0].equalsIgnoreCase("/quit")) {
-
+                return 6;
 			} else if (token[0].charAt(0) == '/') {
 				throw new InputUserNotValid("Comando non valido, digita /help per avere maggiori informazioni");
 			}
