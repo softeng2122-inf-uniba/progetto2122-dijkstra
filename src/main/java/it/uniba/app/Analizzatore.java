4b8a242ab26eaf70d30fcaa81ca407aa32681518
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-05-17 11:29:43 +0200

    aggiunte costanti per la definizione dei comandi disponibili

--- src/main/java/it/uniba/app/Analizzatore.java
+++ src/main/java/it/uniba/app/Analizzatore.java
@@ -3,6 +3,16 @@ package it.uniba.app;
 public final class Analizzatore{
     
     public enum Colore {GRIGIO,GIALLO,VERDE};
+
+    private static String[] token;
+    private static final String stringNuova = new String("/new");
+	private static final String stringMostra = new String("/show");
+	private static final String stringHelp = new String("/help");
+	private static final String stringLessLessH = new String("--h");
+	private static final String stringLessHelp = new String("-h");
+	private static final String stringPlay = new String("/play");
+	private static final String stringExit = new String("/exit");
+	private static final String stringQuit = new String("/quit"); 
     
     public static Colore[] analizzatoreTentativo(String input,String parolaSegreta){
         
@@ -44,18 +54,18 @@ public final class Analizzatore{
 
 		if (token.length <= 2) {
 
-			if (token[0].equalsIgnoreCase("/nuova")) {
+			if (token[0].equalsIgnoreCase(stringNuova)) {
                 return 1;
-			} else if (token[0].equalsIgnoreCase("/mostra")) {
+			} else if (token[0].equalsIgnoreCase(stringMostra)) {
                 return 2;
-			} else if (token[0].equalsIgnoreCase("/help") || token[0].equalsIgnoreCase("--h")
-					|| token[0].equalsIgnoreCase("-help")) {
+			} else if (token[0].equalsIgnoreCase(stringHelp) || token[0].equalsIgnoreCase(stringLessLessH)
+					|| token[0].equalsIgnoreCase(stringLessHelp)) {
                 return 3;
-			} else if (token[0].equalsIgnoreCase("/play")) {
+			} else if (token[0].equalsIgnoreCase(stringPlay)) {
                 return 4;
-			} else if (token[0].equalsIgnoreCase("/exit")) {
+			} else if (token[0].equalsIgnoreCase(stringExit)) {
                 return 5;
-			} else if (token[0].equalsIgnoreCase("/quit")) {
+			} else if (token[0].equalsIgnoreCase(stringQuit)) {
                 return 6;
 			} else if (token[0].charAt(0) == '/') {
 				throw new InputUserNotValid("Comando non valido, digita /help per avere maggiori informazioni");
