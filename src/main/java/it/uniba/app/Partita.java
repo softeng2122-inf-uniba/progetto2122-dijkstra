f10e5fc577986907a6abc22dae99d3d1ca1da7ae
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-05-18 17:09:33 +0200

    Implementazione costruttore della classe partita

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -1,5 +1,22 @@
 package it.uniba.app;
 
 public final class Partita {
+
+    private char[][] matriceTentativi;
     
+    Partita(int numbOfWords, int numbOfTries) {
+        System.out.println("Creazione partita...");
+        
+        matriceTentativi = new char [numbOfTries][numbOfWords];
+        
+        /**inizializzazione matriceTentativi*/
+        for(int i=0; i < numbOfTries; i++){
+            for (int j=0; j < numbOfWords; j++){
+                matriceTentativi[i][j]='_';
+            }
+        }
+         
+        System.out.println("Partita creata! Puoi cominciare a giocare");
+    }
+
 }
