54b33f8ae14e3daacc88bca22371eea76f043bee
alessia.finiguerra <97960373+alefini@users.noreply.github.com>
2022-05-19 16:38:12 +0200

    Sistemata sintassi commento descrizione classe

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -2,8 +2,8 @@ package it.uniba.app;
 
 import java.util.Scanner;
 
-/** questa classe ha il compito di gestire le istanze della classe giocatore e permettere l'input all'utente **/
-/*<<entity>>**/
+/** questa classe ha il compito di gestire le istanze della classe giocatore e permettere l'input all'utente */
+/**<<entity>>*/
 
 public class Giocatore {
     public String input() {
