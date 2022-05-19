80547d104632b6a5b7c235c30b0253fd42094460
Roby2851 <roby2435@gmail.com>
2022-05-19 16:44:26 +0200

    Commentata classe "InputUserNotValid"

--- src/main/java/it/uniba/app/InputUserNotValid.java
+++ src/main/java/it/uniba/app/InputUserNotValid.java
@@ -1,5 +1,9 @@
 package it.uniba.app;
 
+/**Classe per la gestione dei casi in cui giocatore/paroliere inseriscano input
+ invalidi*/
+
+/**<<NoECB>>*/
 class InputUserNotValid extends Exception {
 	InputUserNotValid() {}
 	
