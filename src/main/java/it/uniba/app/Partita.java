bb41487e858513ef6d7872ec1fd8f5497743ca90
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-05-19 11:52:01 +0200

    Aggiunta matrice contenitore dei tentavi effettuati e costruttore di inizializzazione della matrice della partita

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -1,5 +1,24 @@
 package it.uniba.app;
 
 public final class Partita {
-    
+    private String[][] matriceTentativi;
+
+    Partita(int numbOfWords, int numbOfTries) {
+        System.out.println("Creazione partita...");
+
+        matriceTentativi = new String[numbOfTries][numbOfWords];
+        quit = false;
+
+        /**
+         * inizializzazione matriceTentativi
+         */
+        for (int i = 0; i < numbOfTries; i++) {
+            for (int j = 0; j < numbOfWords; j++) {
+                matriceTentativi[i][j] = "_";
+            }
+        }
+
+        stampaMatrice();
+        System.out.println("Partita creata! Puoi cominciare a giocare");
+    }
 }
