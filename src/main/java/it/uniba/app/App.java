105c69192912bcf1da999343c17aeea787a669e4
Chiara Margari <margarichiara2001@gmail.com>
2022-06-01 20:08:09 +0200

    Inserimento messaggio iniziale all'utente riguardante l'help dei comandi

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -100,7 +100,7 @@ public final class App {
                 System.err.println(e.getMessage());
             }  
         } else {
-            getHelp();
+            System.out.println("*** Per avere la lista dei comandi digita '-h' / '--help' ***");
         }
 
         while(true) {
