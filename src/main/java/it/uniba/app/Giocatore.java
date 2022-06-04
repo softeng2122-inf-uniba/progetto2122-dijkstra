a26af7b6f59881f5a9af8f1661253f7cbc9bc3d3
Queek108 <83340599+Andrei-Stefan20@users.noreply.github.com>
2022-06-04 19:24:17 +0200

    Modifiche in giocatore

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -2,15 +2,24 @@ package it.uniba.app;
 
 import java.util.Scanner;
 
-/** questa classe ha il compito di gestire le istanze della classe giocatore e permettere l'input all'utente */
+/** questa classe ha il compito di gestire le istanze della classe 
+ * giocatore e permettere l'input all'utente. 
 /**<<entity>>*/
-
 public class Giocatore {
-    public String input() {
+    
+    /**
+     * Costruttore.
+    */
+    public Giocatore(){
+    }
+    
+    /**
+     * Gestisce input giocatore da terminale.
+     * @return input da terminale
+    */
+    public static String input() {
         Scanner keyboard = new Scanner(System.in);
         String userInput = keyboard.nextLine();
         return userInput;
-    }
-    public Giocatore(){
-    }
+    }  
 }
