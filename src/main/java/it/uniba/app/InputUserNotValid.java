650a7ae90fd794ac5ec653d8e141d6f15eaba243
Chiara Margari <margarichiara2001@gmail.com>
2022-06-04 19:33:30 +0200

    Inserimento commento del metodo e miglioramento struttura commento iniziale

--- src/main/java/it/uniba/app/InputUserNotValid.java
+++ src/main/java/it/uniba/app/InputUserNotValid.java
@@ -1,12 +1,16 @@
 package it.uniba.app;
 
-/**Classe per la gestione dei casi in cui giocatore/paroliere inseriscano input
- invalidi*/
-
-/**<<NoECB>>*/
+/**
+ * Classe per la gestione dei casi in cui giocatore/paroliere inseriscano input invalidi.
+ * <<NoECB>>
+ */
 class InputUserNotValid extends Exception {
 	InputUserNotValid() {}
 	
+        /**
+         * metodo costruttore per il richiamo di una eccezione.
+         * @param msg 
+         */
 	InputUserNotValid(String msg) {
 		super(msg);
 	}
