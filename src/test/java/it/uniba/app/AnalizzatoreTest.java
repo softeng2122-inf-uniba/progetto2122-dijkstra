3dccb82a27ecca1916b89e0f44adf9ae7c3ac321
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-06-07 12:13:41 +0200

    aggiunto metodo per controllo di sintassi di una stringa con spazio

--- src/test/java/it/uniba/app/AnalizzatoreTest.java
+++ src/test/java/it/uniba/app/AnalizzatoreTest.java
@@ -10,4 +10,9 @@ class AnalizzatoreTest {
         assertFalse(Analizzatore.analizzatoreSintattico("p0rt4"));
     }
 
+    @Test
+    void syntaxAnalyseStringWithSpace() {
+        assertFalse(Analizzatore.analizzatoreSintattico("port a"));
+    }
+
 }
