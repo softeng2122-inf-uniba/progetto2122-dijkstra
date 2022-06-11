e484e917776708a44131bf2cafe792259786c2b7
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-06-11 15:47:48 +0200

    Aggiunto test per controllo di sintassi di una stringa alfanumerica

--- src/test/java/it/uniba/app/AnalizzatoreTest.java
+++ src/test/java/it/uniba/app/AnalizzatoreTest.java
@@ -1,7 +1,13 @@
 package it.uniba.app;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnalizzatoreTest {
+
+    @Test
+    void syntaxAnalyseAlphaNumericString() {
+        assertFalse(Analizzatore.analizzatoreSintattico("p0rt4"));
+    }
 
 }
